8c1a99107a503867262e732f80efe8669133046b
Aniket Pandey <aniketp@iitk.ac.in>
2019-03-08 15:32:59 +0530

    Detect the PCI support and realtek chip's presence in the system

--- driver.c
+++ driver.c
@@ -8,10 +8,35 @@
 #include <linux/pci.h>		/* Peripheral Component Interconnect Bus */
 
 
+static struct pci_dev *probe_device(void)
+{
+	struct pci_dev *pdev;
+	if (!pci_present()) {
+		printk(KERN_ERR "PCI BIOS not supported in the system\n");
+		return NULL;
+	}
+
+	if (pdev = pci_find_device(REALTEK_VENDOR_ID, REALTEK_DEVICE_ID, NULL)) {
+		if (pci_enable_device(pdev)) {
+			printk(KERN_ERR "Could not enable the device\n");
+			return NULL;
+		}
+		else {
+			printk(KERN_NOTICE "Device enabled\n");
+		}
+	}
+	else {
+		printk(KERN_ERR "Realtek8139 chip not found\n");
+		return NULL;
+	}
+
+	return pdev;
+}
+
 static __init int init_module(void)
 {
 	struct pci_dev *pdev;
-	if ((pdev = pci_find_device(REALTEK_VENDOR_ID, REALTEK_DEVICE_ID, NULL)) > 0)
+	if ((pdev = probe_device()) != NULL)
 		printk(KERN_INFO "Device detected\n");
 	return 0;
 }
